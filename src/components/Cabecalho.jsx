274420e1b467d377a51f361e3227ff832ff84d7f
santos-nikolas <rm551566@fiap.com>
2023-09-04 09:30:12 -0300

    Aula componentes

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,17 @@
+ 
+export default function Cabecalho(){
+    return(
+        <>
+                    <header>
+          <h1>Vite+React coded by - RM551566</h1>
+          <nav>
+            <ul>
+              <li>Item-1</li>
+              <li>Item-2</li>
+              <li>Item-3</li>
+            </ul>
+          </nav>
+        </header>
+        </>
+    )
+}
