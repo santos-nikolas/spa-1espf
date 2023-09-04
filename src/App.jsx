4ddbd623bdf9fc292f9f8617cbac4466abb7d7b5
santos-nikolas <rm551566@fiap.com>
2023-09-04 09:54:07 -0300

    utilizando props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Cabecalho from "./components/Cabecalho";
 import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
+import viteLogo from "./assets/vite.svg";
 
 export default function App() {
+
+  let viteLogoAlt = "Vite Logo";
+
   return (
     <>
       <h1>APP - Componente principal!</h1>
@@ -11,7 +15,7 @@ export default function App() {
         <Cabecalho />
 
         {/* Iniciando a área do conteúdo */}
-        <Conteudo />
+        <Conteudo viteLogoProps={viteLogo} viteLogoAltProps={viteLogoAlt}/>
 
         {/* Iniciando a área do rodapé */}
         <Rodape />
